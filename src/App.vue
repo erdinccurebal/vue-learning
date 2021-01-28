a2aeb50618d36e6b52800e0ba9507284c15a00ca
Erdinç Cürebal <erdinccurebal@hotmail.com>
2021-01-28 04:01:57 +0300

    update

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,45 @@
 <template>
   <div>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <slot-test>
+      <h1>Title</h1>
+      <template #posts>
+        <div v-for="post in posts" :key="post.id">
+          <h2>{{ post.title }}</h2>
+          <p>{{ post.content }}</p>
+        </div>
+      </template>
+    </slot-test>
   </div>
-  <router-view/>
 </template>
+
+<script>
+import SlotTest from "./components/SlotTest";
+
+export default {
+  components: {
+    SlotTest,
+  },
+  data() {
+    return {
+      posts: [],
+    };
+  },
+  created() {
+    setTimeout(() => {
+      this.posts = [
+        {
+          id: 1,
+          title: "Lorem Title 12312323",
+          content: "Lorem Content 123435341",
+        },
+        { id: 2, title: "Lorem Title 45632", content: "Lorem Content 763121" },
+        {
+          id: 3,
+          title: "Lorem Title 34212131",
+          content: "Lorem Content 3424234",
+        },
+      ];
+    }, 2000);
+  },
+};
+</script>
---
+++ src/components/SlotTest.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <slot></slot>
+    <slot name="posts">
+      <h2>Data Yükleniyor...</h2>
+    </slot>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
